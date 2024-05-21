8fb834f4c94ae913ce14419de79f516daafb48c8
slavyashik <git@slavaorlov.ru>
2024-05-21 16:09:08 +0700

    Добавил проверки: - в clear, delete, get: на ненулевое количество резюме - в save: проверку Resume not null
    Оптимизировал метод delete: убрал лишнюю проверку, включив код сдвига данных в массиве в цикл.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,35 +8,40 @@ public class ArrayStorage {
     private int resumesCount;
 
     void clear() {
-        Arrays.fill(storage, 0, resumesCount, null);
-        resumesCount = 0;
+        if (resumesCount > 0) {
+            Arrays.fill(storage, 0, resumesCount, null);
+            resumesCount = 0;
+        }
     }
 
     void save(Resume r) {
-        storage[resumesCount++] = r;
+        if (r != null) {
+            storage[resumesCount++] = r;
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < resumesCount; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return storage[i];
+        if (resumesCount > 0) {
+            for (int i = 0; i < resumesCount; i++) {
+                if (storage[i].toString().equals(uuid)) {
+                    return storage[i];
+                }
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int index = -1;
+        if (resumesCount == 0) {
+            return;
+        }
 
         for (int i = 0; i < resumesCount; i++) {
             if (storage[i].toString().equals(uuid)) {
-                index = i;
+                System.arraycopy(storage, i + 1, storage, i, resumesCount - i - 1);
+                storage[--resumesCount] = null;
+                break;
             }
-        }
-
-        if (index != -1) {
-            System.arraycopy(storage, index + 1, storage, index, resumesCount - index - 1);
-            storage[--resumesCount] = null;
         }
     }
 

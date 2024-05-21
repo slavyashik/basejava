9ecd454f1f0847edea15fa7e800b73634c429df1
slavyashik <git@slavaorlov.ru>
2024-05-21 15:20:17 +0700

    - Инициализация проекта. - Реализован функционал методов: clear, save, get, delete, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
- * Array based storage for Resumes
+ * Array based storage for Resumes.
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+    private int resumesCount;
 
     void clear() {
+        Arrays.fill(storage, 0, resumesCount, null);
+        resumesCount = 0;
     }
 
     void save(Resume r) {
+        storage[resumesCount++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumesCount; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = -1;
+
+        for (int i = 0; i < resumesCount; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                index = i;
+            }
+        }
+
+        if (index != -1) {
+            System.arraycopy(storage, index + 1, storage, index, resumesCount - index - 1);
+            storage[--resumesCount] = null;
+        }
     }
 
-    /**
+    /*
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, resumesCount);
     }
 
     int size() {
-        return 0;
+        return resumesCount;
     }
 }
